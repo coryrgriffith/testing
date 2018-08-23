9e5d1247c637e02e83bb052ac18ba84d63f039b3
coryrgriffith <corygriffith9@gmail.com>
2018-08-22 19:54:41 -0500

    run tests on a simple calculator class using the rpspec gem

---
+++ calcualtor-test.rb
@@ -0,0 +1,72 @@
+require 'rspec'
+
+class Calculator
+  def add(number_one, number_two)
+    return number_one + number_two
+  end
+
+  def subtract(number_one, number_two)
+    return number_one - number_two
+  end
+
+  def multiply(number_one, number_two)
+    return number_one * number_two
+  end
+
+  def divide(dividend, divisor)
+    return dividend / divisor
+  end
+
+  def square(number)
+    return number * number
+  end
+
+  def power(number, exponent)
+    return number ** exponent
+  end
+end
+
+RSpec.describe Calculator do
+  describe '#add' do
+    it 'should return the sum of two numbers' do
+      calculator = Calculator.new
+      result = calculator.add(1, 3)
+      expect(result).to eq(4)
+    end
+  end
+  describe '#subtract' do
+    it 'should return the difference of the first number and the second number' do
+      calculator = Calculator.new
+      result = calculator.subtract(5, 4)
+      expect(result).to eq(1)
+    end
+  end
+  describe '#multiply' do
+    it 'should return the multiple of number one and number two' do
+      calculator = Calculator.new
+      result = calculator.multiply(2, 3)
+      expect(result).to eq(6)
+    end
+  end
+  describe '#divide' do
+    it 'should return the quotient of the dividend and the divisor' do
+      calculator = Calculator.new
+      result = calculator.divide(10, 2)
+      expect(result).to eq(5)
+    end
+  end
+  describe '#square' do
+    it 'should return the square of a number' do
+      calculator = Calculator.new
+      result = calculator.square(3)
+      expect(result).to eq(9)
+    end
+  end
+  describe '#power' do
+    it 'should return the first number to the power of the second' do
+      calculator = Calculator.new
+      result = calculator.power(2, 5)
+      expect(result).to eq(32)
+    end
+  end
+end
